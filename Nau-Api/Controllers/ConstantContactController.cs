959fdd1886bf843b5139c60fc5c97c453237a4a7
carogers08 <calebbrogers@gmail.com>
2022-03-08 11:11:42 -0600

    work on getmany call

--- Nau-Api/Controllers/ConstantContactController.cs
+++ Nau-Api/Controllers/ConstantContactController.cs
@@ -27,7 +27,8 @@ namespace Nau_Api.Controllers
             _config = config;
         }
 
-        [HttpGet] //Get all contacts that match the given search parameters
+        //Takes in string of list 
+        [HttpGet]
         [Route("getmany/{_lists}/{limit:int}")]
         public async Task<IActionResult> GetMany([FromRoute] string _lists, [FromRoute] int limit)
         {
@@ -35,82 +36,88 @@ namespace Nau_Api.Controllers
             {
                 return new StatusCodeResult((int)HttpStatusCode.Unauthorized);
             }
-            string[] lists = _lists.Split("+");
-            //Construct the request string with parameters
-            string url = _config.ConstantContactUrl + "contacts?";
 
-            //Add parameters to URL
-            if (lists != null && lists.Length > 0)
-            {
-                url += "lists=";
-                foreach (string list in lists)
-                {
-                    url += list + "%2C";
-                }
-                url = url.Substring(0, url.LastIndexOf("%2C"));
-                url += "&";
-            }
-            if (limit > 0)
-            {
-                url += "limit=" + limit.ToString() + "&";
-            }
+            string[] listNames = _lists.Split("+");
 
-            if (url.LastIndexOf("&") > 0) //the case where no parameters are passed
-            {
-                url = url.Substring(0, url.LastIndexOf('&'));
-            }
-
-            //Actually send the request
+            //First get all lists associated with the account.
+            GetListsResponse listsResponse = null;
             try
             {
-                WebRequest request = WebRequest.Create(url);
-                request.Method = "GET";
-                request.ContentType = "application/json";
-
-                using (HttpWebResponse webResponse = (HttpWebResponse)request.GetResponse())
+                using (var httpClient = new HttpClient())
                 {
-                    //Check the response
-                    if (webResponse.StatusCode == HttpStatusCode.OK)
+                    using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://api.cc.email/v3/contact_lists?include_count=false"))
                     {
-                        using (var reader = new StreamReader(webResponse.GetResponseStream()))
-                        {
-                            try
-                            {
-                                var json = reader.ReadToEnd();
-                                _logger.LogDebug("CALEBX - \n" + json);
+                        request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                        request.Headers.TryAddWithoutValidation("Authorization", "Bearer " + _config.Token);
 
-                                var response = JsonConvert.DeserializeObject<GetManyResponse>(json);
-                                return Ok(response.contacts);
-                            }
-                            catch (Exception e)
-                            {
-                                _logger.LogError(e.Message);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        //calebx - there's a lot of possible response besides just 200
-                        //we have to make a decision of how much effort we want to put into error handling. 
+                        var response = await httpClient.SendAsync(request);
+                        string json = await response.Content.ReadAsStringAsync();
+
+                        //This is an object containing all contact lists associated with the account
+                        listsResponse = JsonConvert.DeserializeObject<GetListsResponse>(json);
                     }
                 }
-            }
-            catch (WebException we)
-            {
-                //calebx - this isn't our problem to deal with since we don't know how they log errors
             }
             catch (Exception e)
             {
-                //calebx - this isn't our problem to deal with since we don't know how they log errors
+                //calebx - I don't feel like writing error handling. I'll just log it and return sadness :)
+                _logger.LogError("GetMany::" + e.Message);
+                return new StatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+
+            //Parse the lists we get back to choose the ones requested
+            List<string> listIds = new List<string>();
+            if (listsResponse != null)
+            {
+                foreach (ContactList cl in listsResponse.lists)
+                {
+                    //if the client request contains this list's name, add the list id to listIds for a query
+                    if (listNames.Contains(cl.name))
+                    {
+                        listIds.Add(cl.list_id);
+                    }
+                }
+
+                //Now get all contacts associated with those lists
+                try
+                {
+                    using (var httpClient = new HttpClient())
+                    {
+                        using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://api.cc.email/v3/contacts"))
+                        {
+                            request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                            request.Headers.TryAddWithoutValidation("Authorization", "Bearer " + _config.Token);
+
+                            var contentList = new List<string>();
+                            contentList.Add("limit=" + limit);
+                            contentList.Add(string.Join("+", listIds));
+
+                            request.Content = new StringContent(string.Join("&", contentList));
+                            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+
+                            var response = await httpClient.SendAsync(request);
+                            string json = await response.Content.ReadAsStringAsync();
+
+                            var contacts = JsonConvert.DeserializeObject<GetManyResponse>(json);
+
+                            return Ok(contacts.contacts); //return the list of contacts
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError("GetMany::" + e.Message);
+                }
             }
 
             //If we're here something went wrong so return an empty list.
-            return Ok(new List<Contact>());
+            return new StatusCodeResult((int)HttpStatusCode.BadRequest);
         }
 
         //CALEBX - this is probably not the way it was designed to be done. But I wasn't sure how else to implement it.
         //This method recieves the code that we get back from the OAuth request made in the angular side of the project.
         //It takes the code and exchanges it for a token and other things which we use for any CC-API calls. 
+        //Token is stored in ApiConfiguration object (this._config)
         [HttpPost]
         [Route("authorize")]
         public async Task<IActionResult> Authorize([FromForm] string _code)
@@ -138,7 +145,6 @@ namespace Nau_Api.Controllers
                         request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
 
                         var response = await httpClient.SendAsync(request);
-                        HttpStatusCode temp = response.StatusCode;
                         string json = await response.Content.ReadAsStringAsync();
 
                         var authorization = JsonConvert.DeserializeObject<AuthorizationResponse>(json);
---
+++ Nau-Api/Models/ContactList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nau_Api.Models
+{
+    public class ContactList
+    {
+        public string list_id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public bool favorite { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public int membership_count { get; set; }
+    }
+}
